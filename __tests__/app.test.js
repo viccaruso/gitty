14fd4ecc07092879b765ed9c2a9f7e28032c3231
Victor Caruso <vic.caruso@gmail.com>
2022-03-31 15:48:17 -0700

    Establish app route to /api/v1/github

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -3,6 +3,8 @@ const setup = require('../data/setup');
 const request = require('supertest');
 const app = require('../lib/app');
 
+jest.mock('../lib/utils/github');
+
 describe('gitty routes', () => {
   beforeEach(() => {
     return setup(pool);
