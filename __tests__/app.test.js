9d9876b35a693169317bdaeb37d4595a42bd89c1
Victor Caruso <vic.caruso@gmail.com>
2022-04-01 19:31:40 -0700

    Establish POST route to insert posts to posts table, set up insert method on model

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -16,6 +16,7 @@ describe('gitty routes', () => {
 
   it('Should redirect to the github oauth page', async () => {
     const res = await request(app).get('/api/v1/github/auth');
+
     expect(res.header.location).toMatch(
       /https:\/\/github.com\/login\/oauth\/authorize\?client_id=[\w\d]+&scope=user&redirect_uri=http:\/\/localhost:7890\/api\/v1\/github\/auth\/callback/i
     );
@@ -26,6 +27,7 @@ describe('gitty routes', () => {
       .agent(app)
       .get('/api/v1/github/auth/callback?code=42')
       .redirects(1);
+
     expect(res.req.path).toEqual('/api/v1/posts');
   });
 
@@ -37,4 +39,30 @@ describe('gitty routes', () => {
       status: 401
     });
   });
+
+  it('Should return 401 Unauthorized if a user tries to create a post while not logged in', async () => {
+    const res = await request(app).post('/api/v1/posts').send({ post: 'This is a test.' });
+    expect(res.body).toEqual({
+      message: 'You must be signed in to continue.',
+      status: 401
+    });
+  });
+
+  // it('Should return a list of all posts as long as a user is logged in', async () => {
+  //   const agent = await request.agent(app);
+  //   await agent.get('/api/v1/github/auth/callback?code=42').redirects(1);
+  //   const res = await agent.get('/api/v1/posts');
+  // expect(res.body).toEqual(
+  //   [
+  //     { id: "1", post: "This is a post." }, { id: "2", post: "This is another post." }
+  //   ]
+  // );
+  // });
+
+  // it('Should allow a user to create a post as long as a user is logged in', async () => {
+  //   const agent = await request.agent(app);
+  //   await agent.get('/api/v1/github/auth/callback?code=42').redirects(1);
+  //   const res = await agent.post('/api/v1/posts').send({ post: 'This is a test.' });
+  //   expect(res.body).toEqual({ id: expect.any(String), post: "This is a post." });
+  // });
 });
